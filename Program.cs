ba74f6ea068f96e525e93666d8941f4ff48aa577
pnrbrk41 <113897255+pnrbrk41@users.noreply.github.com>
2022-09-23 23:22:51 +0300

    duzenlendi
    duzenlendi

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+	namespace switchh
+	{
+
+
+   class Program
+   
+   {
+     static void Main(string[] args){
+     
+     Console.Write("Lütfen bir sayı giriniz: ");
+     int sayi= int.Parse(Console.ReadLine());
+     int sayac = 1;
+     int toplam = 0;
+     while( sayac <= sayi)
+     {
+        toplam += sayac;
+        sayac ++;
+
+     }
+     Console.WriteLine(toplam/sayi);
+
+
+     // a dan z ye kadar olan tüm sayıları yazdır
+     char character = 'a';
+     while( character < 'z')
+
+     {
+        Console.WriteLine(character);
+     character++;
+            
+     }
+
+   }}
+
+     }
